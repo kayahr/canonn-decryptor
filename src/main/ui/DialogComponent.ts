8fe9cfbb4b91f06432cd706b28fc30f1ed6c0682
Klaus Reimer <k@ailis.de>
2025-09-01 01:27:46 +0200

    Add missing file headers

--- src/main/ui/DialogComponent.ts
+++ src/main/ui/DialogComponent.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2017 Klaus Reimer <k@ailis.de>
+ * See LICENSE.md for licensing information.
+ */
+
 import { Component } from "@angular/core";
 
 import template from "../../../assets/ui/dialog.html?raw";
--- src/main/utils/Quadgrams.ts
+++ src/main/utils/Quadgrams.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2017 Klaus Reimer <k@ailis.de>
+ * See LICENSE.md for licensing information.
+ */
+
 import quadgramsJSON from "../../../assets/fitness/ngrams.json" with { type: "json" };
 import { FastString } from "../utils/FastString.js";
 
